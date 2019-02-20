d1e4c8cea8002a6efa3910908d16034e3551b982
Ravi Saini <mailravi.saini@yahoo.com>
2019-02-20 15:44:14 +0530

    Adding the changes

--- Testing/src/FrequencyOfElementInCollection.java
+++ Testing/src/FrequencyOfElementInCollection.java
@@ -5,9 +5,19 @@ public class FrequencyOfElementInCollection {
 
 	public static void main(String[] args) {
 		ArrayList<String> list = new ArrayList<String>();
+		ArrayList<String> list2 = new ArrayList<String>();
 		list.add("Ravi");
-		list.add("Ravi");
+		list.add("Saini");
+		list.add("A");
+
+		list2.add("Deepali");
+		list2.add("Rathore");
+		list2.add("B");
+
 		System.out.println(Collections.frequency(list, "Ravi"));
+		System.out.println(Collections.binarySearch(list, "Saini"));
+		Collections.copy(list, list2);
+		Collections.rotate(list2, 3);
 
 	}
 
